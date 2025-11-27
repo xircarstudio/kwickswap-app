a7b4b39083af4a9610cfc7eab14af38f65ea2b1b
Favour Shaw <favourvanderwaal@gmail.com>
2025-11-27 01:08:22 +0100

    Home Header Implementation

--- app/(dash)/index.tsx
+++ app/(dash)/index.tsx
@@ -1,11 +1,15 @@
+import HomeHead from "@/components/homeHead";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-primary">
-      <Text className="text-success">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <>
+      <HomeHead />
+      <View className="flex-1 justify-center items-center bg-primary">
+        <Text className="text-success">
+          Edit app/index.tsx to edit this screen.s
+        </Text>
+      </View>
+    </>
   );
 }
---
+++ components/homeHead.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+const HomeHead = () => {
+  return (
+    <View className="h-[88px] bg-primary flex-row justify-between items-center">
+      <View>
+        <Image source={require("@/assets/img/profile.png")} />
+      </View>
+      <Text className="text-white">hi</Text>
+    </View>
+  );
+};
+
+export default HomeHead;
+
+const styles = StyleSheet.create({});

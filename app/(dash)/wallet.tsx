3cd6263e31886e7f5c8d6589f13f3b43968a33ed
Favour Shaw <favourvanderwaal@gmail.com>
2025-12-15 09:54:24 +0100

    Base UI implementation

--- app/(dash)/wallet.tsx
+++ app/(dash)/wallet.tsx
@@ -1,11 +1,37 @@
+import { colors } from "@/assets/styles/styles";
+import HomeHead from "@/components/home/homeHead";
+import TopGradientCard from "@/components/home/TopGradientCard";
+import WalletItems from "@/components/wallet/WalletItems";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const wallet = () => {
   return (
-    <View>
-      <Text>wallet</Text>
-    </View>
+    <SafeAreaView className="flex-1 justify-start  bg-primary px-container">
+      <HomeHead />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="mt-8">
+          <TopGradientCard
+            balance="$42,593.40"
+            label="Portfolio Value"
+            labelIcon="briefcase-sharp"
+            stats={[
+              {
+                icon: "trending-up",
+                iconColor: colors.secondary,
+                text: "+12.3%",
+                subText: "24H",
+                bg: colors.secondary + 20,
+                border: colors.secondary + 40,
+                textColor: colors.secondary,
+              },
+            ]}
+          />
+        </View>
+        <WalletItems />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 

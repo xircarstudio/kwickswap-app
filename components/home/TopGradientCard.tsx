cf35b4b196ae70b6f65232d3b911eca13b913098
Favour Shaw <favourvanderwaal@gmail.com>
2025-12-15 09:51:05 +0100

    UI Fix

--- components/home/TopGradientCard.tsx
+++ components/home/TopGradientCard.tsx
@@ -2,7 +2,7 @@ import { colors } from "@/assets/styles/styles";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 interface StatItem {
   icon: keyof typeof Ionicons.glyphMap;
@@ -35,15 +35,7 @@ export default function TopGradientCard({
       colors={gradientColors}
       start={{ x: 0, y: 0.5 }}
       end={{ x: 1, y: 0 }}
-      style={{
-        borderRadius: 29,
-        padding: 32,
-        gap: 16,
-        alignItems: "center",
-        position: "relative",
-        borderWidth: 1,
-        borderColor: colors.muted + "20",
-      }}
+      style={styles.gradient}
     >
       <View className="flex-row gap-5 items-center bg-muted/10 border border-muted/40 rounded-3xl px-8 py-2">
         <Text className="text-muted">{label}</Text>
@@ -83,3 +75,15 @@ export default function TopGradientCard({
     </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  gradient: {
+    borderRadius: 29,
+    padding: 32,
+    gap: 16,
+    alignItems: "center",
+    position: "relative",
+    borderWidth: 1,
+    borderColor: colors.muted + 20,
+  },
+});

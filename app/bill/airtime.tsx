2f5b17acd7a9f39e18dafff6662215c84bc72cb4
Favour Shaw <favourvanderwaal@gmail.com>
2025-12-19 03:25:13 +0100

    Refactor Airtime component to include network selection buttons and update layout

--- app/bill/airtime.tsx
+++ app/bill/airtime.tsx
@@ -1,10 +1,45 @@
+import { colors } from "@/assets/styles/styles";
+import IconButtonGrid, { IconButtonItem } from "@/components/ui/IconButtonGrid";
 import React from "react";
 import { Text, View } from "react-native";
 
+const BUTTONS: IconButtonItem[] = [
+  {
+    label: "MTN",
+    image: require("@/assets/images/icon.png"),
+    route: "/",
+    color: colors.warning,
+  },
+
+  {
+    label: "GLO",
+    image: require("@/assets/images/icon.png"),
+    route: "/(sub)/buy",
+    color: colors.success,
+  },
+
+  {
+    label: "Airtel",
+    image: require("@/assets/images/icon.png"),
+    route: "/(sub)/buy",
+    color: colors.danger,
+  },
+
+  {
+    label: "9mobile",
+    image: require("@/assets/images/icon.png"),
+    route: "/(sub)/buy",
+    color: colors.success,
+  },
+];
+
 const Airtime = () => {
   return (
-    <View className="flex-1 bg-primary py-6">
-      <Text>A</Text>
+    <View className="flex-1 bg-primary py-6 px-container">
+      <Text className="text-white font-semibold text-lg mb-4">
+        Select Network
+      </Text>
+      <IconButtonGrid buttons={BUTTONS} columns={2} itemSize={170} />
     </View>
   );
 };
